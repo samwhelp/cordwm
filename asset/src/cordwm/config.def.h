af4b5ee46048b3b42d8c14ff0f52362196ead671
samwhelp <samwhelp@users.noreply.github.com>
2021-10-22 00:58:12 +0800

    mfact = 0.80

--- asset/src/cordwm/config.def.h
+++ asset/src/cordwm/config.def.h
@@ -88,7 +88,7 @@ static const int ulineall                    = 0;           /* 1 to show underli
 
 
 /* layout(s) */
-static const float mfact                     = 0.50;        /* factor of master area size [0.05..0.95] */
+static const float mfact                     = 0.80;        /* factor of master area size [0.05..0.95] */
 static const int nmaster                     = 1;           /* number of clients in master area */
 static const int resizehints                 = 0;           /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen              = 1;           /* 1 will force focus on the fullscreen window */
